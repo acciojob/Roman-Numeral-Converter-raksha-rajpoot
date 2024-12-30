5757c9a11f4e235285e69df03ced581bd0d34e40
raksha-rajpoot <rakraj654@gmail.com>
2024-12-30 18:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 function convertToRoman(num) {
 	
-  	Your function looks like it should work for most cases, but there’s one issue: it doesn’t handle the special cases where a smaller numeral appears before a larger one, such as 4 (‘IV’), 9 (‘IX’), 40 (‘XL’), 90 (‘XC’), 400 (‘CD’), and 900 (‘CM’).
-
-In your current setup, these numbers would be represented as ‘IIII’, ‘VIIII’, ‘XXXX’, ‘LXXXX’, ‘CCCC’, and ‘DCCCC’, respectively, which are not correct.
-
-The code block you’ve added after the while loop attempts to handle this, but it will only work for the cases of 4 (‘IV’) and 9 (‘IX’). It won’t handle the cases of 40, 90, 400, and 900 correctly.
-
-Instead, you can handle these cases by adding special entries for them in your ‘obj’ array. Here’s how you can modify it:
-
-const obj = {
-  0:['M',1000], 
-  1:['CM',900],
-  2:['D', 500], 
-  3:['CD',400],
-  4:['C', 100], 
-  5:['XC',90],
-  6:['L', 50], 
-  7:['XL',40],
-  8:['X', 10], 
-  9:['IX',9],
-  10:['V', 5], 
-  11:['IV',4],
-  12:['I', 1]
-};
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
  
   //your code here
  let roman = "";
@@ -30,10 +16,10 @@ const obj = {
     for (let key in obj) {
         const [symbol, value] = obj[key];
 
-        while (num >= value) {
-            roman += symbol; 
-            num -= value;   
-        }
+        // while (num >= value) {
+        //     roman += symbol; 
+        //     num -= value;   
+        // }
 
         if (key % 2 === 0 && key < 6) { 
             const nextSymbol = obj[parseInt(key) + 2][0]; 
